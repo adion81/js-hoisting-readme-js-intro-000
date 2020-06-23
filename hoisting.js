4a195d0c69e54350b2a8f902e17be2d85dc41229
Adrien Joseph Dion <adion81@gmail.com>
2020-06-23 02:31:00 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,16 +11,17 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
+
 
   sayMy();
 
